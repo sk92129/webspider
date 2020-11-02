8978cdb35680ba1d3ee672ac36f8251623582152
Sean Kang <seankang@gmail.com>
2020-11-02 00:44:59 -0800

    recursively visit internal pages

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # webspider
 Point to a web domain.
 Crawls the whole domain for all its pages, images, and other external links.
+
+## Instructions
+1. Install nodejs 
+2. run "node index.js" 
+
+
+## References
+
+https://www.twilio.com/blog/web-scraping-and-parsing-html-with-node-js-and-cheerio
+
--- index.js
+++ index.js
@@ -1,22 +1,54 @@
-var request = require('request');
+
 var cheerio = require('cheerio');
+var got = require('got')
+var loki = require('lokijs')
+
+var db = new loki('sandbox.db');
+
+var urlRoot = "https://wiprodigital.com";
+
+// Using in-memory database to keep track of which web links we have already visited 
+// because this is visiting links recursively.
+
+var webitems = db.addCollection('webitems');
+
+var recursiveGet = function(urlParent, urlToFetch) {
+    got(urlToFetch).then(response => {
+      const $ = cheerio.load(response.body);
+      // handle the links <a>
+      $('a').each((i, link) => {
+        const href = link.attribs.href;
+        if (href.indexOf(urlRoot) == 0) {
+          // internal link
+          var existsitem = webitems.findOne({'url': href});
+          if (existsitem === null){
+            // this means that we havent visted that page yet.
+            console.log("internal web : "   + href);
+            webitems.insert({ urlparent : urlToFetch, url: href });
+            recursiveGet(urlToFetch, href);
+          }
+          else {
+            //console.log("already visited")
+          }
+        } else {
+          console.log("web link external: " + href);
+        }
+      });
+      // handle the images 
+      $('img').each((i, link) => {
+        const imageSrc = link.attribs.src;
+        
+        console.log("image src : " + imageSrc);
+        
+      });
 
 
-var url = "http://wikipedia.org";
+    }).catch(err => {
+      console.log(err);
+    });
+  
+} 
 
-request(url, function (error, response, body) {
-  if (!error) {
-    var $ = cheerio.load(body)
+webitems.insert({ urlparent : '', url: urlRoot });
+recursiveGet("", urlRoot);
 
-    var title = $('title').text();
-    var content = $('body').text();
-    var freeArticles = $('.central-featured-lang.lang1 a small').text()
-
-    console.log('URL: ' + url);
-    console.log('Title: ' + title);
-    console.log('EN articles: ' + freeArticles);
-  }
-  else {
-    console.log("We’ve encountered an error: " + error);
-  }
-});
